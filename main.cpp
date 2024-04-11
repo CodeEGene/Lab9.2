85de24cc9f3a585ba8015278cebec8a370e57c24
ericschmidutz <e-schmidutz@onu.edu>
2024-04-11 02:40:55 -0400

    Completed isSortedDecreasing

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main() {
 		cin >> numArray[i];
 	}
 
-	cout << isSortedIncreasing(numArray, 6);
+	cout << isSortedDecreasing(numArray, 6);
 
 	return 0;
 }
@@ -37,7 +37,16 @@ bool isSortedIncreasing(int values[], int size) {
 }
 
 bool isSortedDecreasing(int values[], int size) {
-	return false;
+	int placeHolder = values[0];
+	for (int i = 1; i < size; i++) {
+		if (numArray[i] < placeHolder || numArray[i] == placeHolder) {
+			placeHolder = numArray[i];
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
 }
 bool hasAdjecentDuplicates(int values[], int size) {
 	return false;
